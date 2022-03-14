687758f166895985af142517a19bf0bad60aef5c
unknown <SQLI@OUJFF3H7T2.smb.sqli.com>
2022-03-14 17:12:18 +0100

    CALC_1-modified

---
+++ src/main/java/calc/class1.java
@@ -0,0 +1,11 @@
+package calc;
+
+public class class1 {
+
+
+
+    public static float add(float a, float b){return 0;}
+    public static float substracte(float a, float b){return 0;}
+    public static float multiple(float a, float b){return 0;}
+    public static float division(float a, float b){return 0;}
+}

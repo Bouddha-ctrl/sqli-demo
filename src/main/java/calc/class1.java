7e81f01660d4edd3f1dc07b8bad72465bfb059ca
unknown <SQLI@OUJFF3H7T2.smb.sqli.com>
2022-03-14 18:14:19 +0100

    fix4

--- src/main/java/calc/class1.java
+++ src/main/java/calc/class1.java
@@ -7,5 +7,8 @@ public class class1 {
     public static float add(float a, float b){return 0;}
     public static float substracte(float a, float b){return 0;}
     public static float multiple(float a, float b){return 0;}
-    public static float division(float a, float b){return 0;}
+    //divition--fix
+    public static float division(float a, float b){
+        if (b==0) return -1;
+        return 0;}
 }
